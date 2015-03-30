179d04ba19c52aeff3f0138708e76623400ef966
F74036158 <F74036158@mail.ncku.edu.tw>
2015-03-30 20:40:22 +0800

    lab2

---
+++ lab2.h
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class BMI
+	{
+	public:
+		void seth(float);
+		void setm(float);
+		float geth();
+		float getm();
+		float cul();
+		string com(float);
+	private:
+		float h,m;
+	};
+
